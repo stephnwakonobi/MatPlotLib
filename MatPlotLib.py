2bdd7cefbbc6d11ab63e243e33c2398f71b74ef2
stephnwakonobi <stephanienwakonobi@gmail.com>
2024-07-14 00:32:33 +0100

    Initial commit

---
+++ MatPlotLib.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([5, 2, 1, 4, 8, 4, 10, 15])
+ypoints = np.array([3, 1, 3, 5, 10, 3, 9, 7])
+
+plt.plot(xpoints, ypoints)
+plt.show()
